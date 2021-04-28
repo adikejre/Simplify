e0bce90e7c511736f3958fb1f7198a1587d6199d
adi <adityakejrewal2000@gmail.com>
2021-04-28 20:02:34 +0530

    moods

--- app/src/main/java/com/example/myatlabproject1/HomePage.java
+++ app/src/main/java/com/example/myatlabproject1/HomePage.java
@@ -13,11 +13,13 @@ import com.google.firebase.auth.FirebaseAuth;
 public class HomePage extends AppCompatActivity {
     private Button lgout;
     private FirebaseAuth auth;
+    private Button mood;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         lgout=findViewById(R.id.logout);
+        mood=findViewById(R.id.button);
         auth=FirebaseAuth.getInstance();
         lgout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +27,12 @@ public class HomePage extends AppCompatActivity {
                 auth.signOut();
                 Toast.makeText(HomePage.this, "Logged Out!", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(HomePage.this,MainActivity.class));
+            }
+        });
+        mood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this,mood.class));
             }
         });
     }
